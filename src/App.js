52c84b66dff3bff16f2ec8ac530d71904c1cf19d
Nyrok <47859726+Nyrok@users.noreply.github.com>
2022-11-20 23:03:30 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>BeStudy.</h1>
+        <img src="https://play-lh.googleusercontent.com/zMiGwnxDPRH0pseGALVVc6T7JdvdfpDoygm8Ska7m4fFKJ3hodY3DfwCjT-UiJUy6g=w600-h300-pc0xffffff-pd" className="App-logo" alt="logo" />
+        <p>
+           <code>Télécharger</code> l'application <a src='https://google.com' className='App-link'>ici</a>
+        </p>
+      </header>
+    </div>
+  );
+}
