fc50aca5712a43d94daad50f7dbb204bc0a21344
Nyrok <47859726+Nyrok@users.noreply.github.com>
2022-12-19 00:23:55 +0100

    Add DB + Friend System

--- src/pronote/api.js
+++ src/pronote/api.js
@@ -1,49 +1,18 @@
-import Pronote, { fetchInfos, fetchTimetable, login as connect } from '@dorian-eydoux/pronote-api';
-import Express from 'express'
+import { PronoteSession, fetchInfos, fetchTimetable, casUrls, login as connect } from '@dorian-eydoux/pronote-api';
+import Express from 'express';
+import { query } from './db';
 
-/** @type {PropertiesHash} */
-let sessions = {};
+/** @type {number: {PronoteSession}} */
+let sessionsObjects = {};
+/** @type {string: number} */
+let sessionsINE = {};
 
-const casList = {
-    'ac-besancon': 'cas.eclat-bfc.fr',
-    'ac-bordeaux': 'mon.lyceeconnecte.fr',
-    'ac-bordeaux2': 'ent2d.ac-bordeaux.fr',
-    'ac-caen': 'fip.itslearning.com',
-    'ac-clermont': 'cas.ent.auvergnerhonealpes.fr',
-    'ac-dijon': 'cas.eclat-bfc.fr',
-    'ac-grenoble': 'cas.ent.auvergnerhonealpes.fr',
-    'ac-lille': 'cas.savoirsnumeriques62.fr',
-    'ac-lille2': 'teleservices.ac-lille.fr',
-    'ac-limoges': 'mon.lyceeconnecte.fr',
-    'ac-lyon': 'cas.ent.auvergnerhonealpes.fr',
-    'ac-montpellier': 'cas.mon-ent-occitanie.fr',
-    'ac-nancy-metz': 'cas.monbureaunumerique.fr',
-    'ac-nantes': 'cas3.e-lyco.fr',
-    'ac-orleans-tours': 'ent.netocentre.fr',
-    'ac-poitiers': 'mon.lyceeconnecte.fr',
-    'ac-reims': 'cas.monbureaunumerique.fr',
-    'ac-rouen': 'nero.l-educdenormandie.fr',
-    'ac-strasbourg': 'cas.monbureaunumerique.fr',
-    'ac-toulouse': 'cas.mon-ent-occitanie.fr',
-    'ac-valdoise': 'cas.moncollege.valdoise.fr',
-    'agora06': 'cas.agora06.fr',
-    'arsene76': 'cas.arsene76.fr',
-    'atrium-sud': 'www.atrium-sud.fr',
-    'cybercolleges42': 'cas.cybercolleges42.fr',
-    'eure-normandie': 'cas.ent27.fr',
-    'haute-garonne': 'cas.ecollege.haute-garonne.fr',
-    'hdf': 'enthdf.fr',
-    'iledefrance': 'ent.iledefrance.fr',
-    'moncollege-essonne': 'www.moncollege-ent.essonne.fr',
-    'laclasse': 'www.laclasse.com',
-    'ljr-munich': 'cas.kosmoseducation.com',
-    'lyceeconnecte': 'mon.lyceeconnecte.fr',
-    'parisclassenumerique': 'ent.parisclassenumerique.fr',
-    'portail-famille': 'seshat.ac-orleans-tours.fr:8443',
-    'seine-et-marne': 'ent77.seine-et-marne.fr',
-    'somme': 'college.entsomme.fr',
-    'toutatice': 'www.toutatice.fr',
-    'monbureaunumerique-educonnect': 'cas.monbureaunumerique.fr'
+const sessionToINE = (sessionId) => {
+    for(const data of Object.entries(sessionsINE)){
+        if(sessionId !== data[1]) continue;
+        return data[0];
+    }
+    return null;
 }
 
 /**
@@ -52,7 +21,7 @@ const casList = {
  * @returns {void}
  */
 export function cas(req, res){
-    return res.status(200).json(Object.entries(casList).map(cas => {
+    return res.status(200).json(Object.entries(casUrls).map(cas => {
         return {label: cas[1], value: cas[0]}
     })).end();
 }
@@ -68,12 +37,13 @@ export async function login(req, res){
         res.statusMessage = "Bad Request";
         return res.status(400).end();
     }
-    /** @type {Pronote.PronoteSession} */
+    /** @type {PronoteSession} */
     const session = await connect(`https://${rne}.index-education.net/pronote/`, username, password, cas).catch(error => res.status(404).json(error));
+    const ine = (await fetchInfos(session))?.numeroINE;
     session.setKeepAlive(true);
-    sessions[session.id] = session;
+    sessionsObjects[session.id] = session;
+    sessionsINE[ine] = session.id;
     return res.status(200).json(session.id).end();
-
 }
 
 /**
@@ -82,16 +52,17 @@ export async function login(req, res){
  * @returns {void}
  */
 export async function timetable(req, res){
-    const [session, target, from] = [req.query['session'], req.query['target'], req.query['from']];
-    if(!session || !target || !from){
+    const [session, target] = [req.query['session'], req.query['target']];
+    if(!session || !target){
         res.statusMessage = "Bad Request";
         return res.status(400).end();
     }
-    else if (!sessions[session]){
+    else if (!sessionsObjects[session]){
         res.statusMessage = "Forbidden";
         return res.status(403).end();
     }
-    return res.status(200).json(await fetchTimetable(sessions[session])).end();
+    const timetable = await fetchTimetable(sessionsObjects[sessionsINE[target]]);
+    return res.status(200).json(timetable).end();
 }
 
 /**
@@ -105,11 +76,11 @@ export async function infos(req, res){
         res.statusMessage = "Bad Request";
         return res.status(400).end();
     }
-    else if (!sessions[session]){
+    else if (!sessionsObjects[session]){
         res.statusMessage = "Forbidden";
         return res.status(403).end();
     }
-    return res.status(200).json(await fetchInfos(sessions[session])).end();
+    return res.status(200).json(await fetchInfos(sessionsObjects[session])).end();
 }
 
 /**
@@ -118,14 +89,37 @@ export async function infos(req, res){
  * @returns {void}
  */
 export async function addFriend(req, res){
-    const [target, from] = [req.query['target'], req.query['from']];
-    if(!target || !from){
+    const [session, target] = [req.query['session'], req.query['target']];
+    if(!session || !target){
         res.statusMessage = "Bad Request";
         return res.status(400).end();
     }
-    else if (!sessions[session]){
+    else if (!sessionsObjects[session]){
         res.statusMessage = "Forbidden";
         return res.status(403).end();
     }
     return res.status(200).end();
+}
+ 
+/**
+ * @param {Express.Request} req 
+ * @param {Express.Response} res 
+ * @returns {void}
+ */
+export async function getFriends(req, res){
+    const session = req.query['session'];
+    const ine = sessionToINE(session);
+    if(!session){
+        res.statusMessage = "Bad Request";
+        return res.status(400).end();
+    }
+    else if (!sessionsObjects[session] || !ine){
+        res.statusMessage = "Forbidden";
+        return res.status(403).end();
+    }
+    const response = query(`SELECT * FROM Relations WHERE '${ine}' IN (first, second);`).catch(error => {
+        res.statusMessage = error;
+        return res.status(500).end();
+    });
+    return res.status(200).json(response).end();
 }
--- src/pronote/db.js
+++ src/pronote/db.js
@@ -11,12 +11,15 @@ const db = mysql.createConnection({
 export function connect(){
     db.connect(function(err) {
         if (err) throw err;
-        multiQuery(
+        return multiQuery(
             'CREATE TABLE IF NOT EXISTS Adresses(id INTEGER UNIQUE NOT NULL AUTO_INCREMENT,adresse1 VARCHAR(256) NOT NULL,adresse2 VARCHAR(256) NOT NULL,adresse3 VARCHAR(256) NOT NULL,adresse4 VARCHAR(256) NOT NULL,cp VARCHAR(6) NOT NULL,ville VARCHAR(256) NOT NULL,province VARCHAR(256) NOT NULL,pays VARCHAR(256) NOT NULL,PRIMARY KEY (id));', 
-            'CREATE TABLE IF NOT EXISTS Users(ine VARCHAR(11) NOT NULL UNIQUE,nom VARCHAR(256) NOT NULL,prenom VARCHAR(256) NOT NULL,photo VARCHAR(2048) DEFAULT "https://i.imgur.com/tbvsqWK.png",tel VARCHAR(15) DEFAULT NULL,email VARCHAR(320) DEFAULT NULL,id_adresse INTEGER DEFAULT NULL,PRIMARY KEY (ine),FOREIGN KEY (id_adresse) REFERENCES Adresses(id));', 
-            'CREATE TABLE IF NOT EXISTS Credentials(ine VARCHAR(11) NOT NULL UNIQUE,username VARCHAR(32) NOT NULL UNIQUE,password VARCHAR(32) NOT NULL,cas VARCHAR(32) NOT NULL,rne VARCHAR(8) NOT NULL);'
+            'CREATE TABLE IF NOT EXISTS Credentials(ine VARCHAR(11) NOT NULL UNIQUE,username VARCHAR(32) NOT NULL UNIQUE,password VARCHAR(32) NOT NULL,cas VARCHAR(32) NOT NULL,rne VARCHAR(8) NOT NULL);',
+            'CREATE TABLE IF NOT EXISTS Users(ine VARCHAR(11) NOT NULL UNIQUE,nom VARCHAR(256) NOT NULL,prenom VARCHAR(256) NOT NULL,photo VARCHAR(2048) DEFAULT "https://i.imgur.com/tbvsqWK.png",tel VARCHAR(15) DEFAULT NULL,email VARCHAR(320) DEFAULT NULL,id_adresse INTEGER DEFAULT NULL,PRIMARY KEY (ine),FOREIGN KEY (ine) REFERENCES Credentials(ine),FOREIGN KEY (id_adresse) REFERENCES Adresses(id));',
+            'CREATE TABLE IF NOT EXISTS Relations(first VARCHAR(11) NOT NULL UNIQUE,second VARCHAR(11) NOT NULL UNIQUE,timetable BIT DEFAULT 0,marks BIT DEFAULT 0,homeworks BIT DEFAULT 0,date DATETIME,PRIMARY KEY (first, second));',
+            'CREATE TABLE IF NOT EXISTS Pendings(first VARCHAR(11) NOT NULL UNIQUE,second VARCHAR(11) NOT NULL UNIQUE,date DATETIME,PRIMARY KEY (first, second));',
         );
     });
+    console.log("DB Connected")
 }
 
 export function query(query, values = []){
@@ -27,6 +30,9 @@ export function query(query, values = []){
     return null;
 }
 
+/**
+ * @param {...string} queries 
+ */
 export function multiQuery(...queries){
     for(const q of queries) query(q);
 }
