4f520c4706c6767e941e947b7a33e19d9d6549b8
Nyrok <47859726+Nyrok@users.noreply.github.com>
2022-12-31 12:01:28 +0100

    Fix: PROTOCOL_PACKETS_OUT_OF_ORDER

--- src/pronote/db.js
+++ src/pronote/db.js
@@ -1,6 +1,6 @@
 import mysql from 'mysql';
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: "37.187.37.122",
     user: "admin",
     password: "NyrokGames75",
@@ -9,7 +9,7 @@ const db = mysql.createConnection({
 });
 
 export async function connect(){
-    db.connect(function(err) {
+    db.getConnection(function(err, connection) {
         if (err) throw err;
         multiQuery(
             'CREATE TABLE IF NOT EXISTS Adresses(id INTEGER UNIQUE NOT NULL AUTO_INCREMENT,adresse1 VARCHAR(256) NOT NULL,adresse2 VARCHAR(256) NOT NULL,adresse3 VARCHAR(256) NOT NULL,adresse4 VARCHAR(256) NOT NULL,cp VARCHAR(6) NOT NULL,ville VARCHAR(256) NOT NULL,province VARCHAR(256) NOT NULL,pays VARCHAR(256) NOT NULL,PRIMARY KEY (id));', 
@@ -29,10 +29,12 @@ export async function connect(){
  * @returns {Array}
  */
 export async function query(query, callback = undefined, values = []){
-    return await db.query(query, [values], function(err, results){
-        if(err) throw err;
-        if(callback) return callback(results);
-    });
+    return await db.getConnection(function(err, connection) {
+        connection.query(query, [values], function(err, results){
+            if(err) throw err;
+            if(callback) return callback(results);
+        });
+    })
 }
 
 /**
